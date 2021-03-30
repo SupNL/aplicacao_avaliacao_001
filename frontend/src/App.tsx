3572b1731e557346dee6eaf11346ecc579cde58f
NL <gustavoonishinl@gmail.com>
2021-03-30 19:55:09 -0300

    update: include routes on app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import RouteList from './routes'
 
-function App() {
+const App = () => {
   return (
-    <p>
-      Teste inicial
-    </p>
+    <Router>
+      <RouteList />
+    </Router>
   );
 }
 
