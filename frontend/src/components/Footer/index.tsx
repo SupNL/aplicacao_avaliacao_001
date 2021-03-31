ac285e16f9b2c3b32b3f4ec0a72c4fb419bfd382
NL <gustavoonishinl@gmail.com>
2021-03-31 11:11:25 -0300

    add: example footer with style

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './style.css';
+
+const Footer = () => {
+    return (
+        <footer>
+            <h1>Footer exemplo</h1>
+        </footer>
+    )
+}
+
+export default Footer;
